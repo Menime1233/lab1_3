b2eae93195df00dcb58824d57dbb8640598158d9
Hydrogenuine <kuchev444@gmail.com>
2026-04-16 00:07:24 +0500

    отредактировал lib.py

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
 def count_common_elements(*lists):
     sets = [set(lst) for lst in lists ]
-
+    #преобзразует все списки в множества
     common_elements = set.intersection(*sets)
     return len(common_elements)
